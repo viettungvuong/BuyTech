732f86afd875f70d0204417e566bf13843844983
viettungvuong <viettungvuong@gmail.com>
2023-06-06 19:45:44 +0700

    Import

--- app/src/main/java/com/tung/buytech/MainActivity.kt
+++ app/src/main/java/com/tung/buytech/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.GridLayout
 import com.google.android.material.textfield.TextInputEditText
+
+import com.google.firebase.ktx.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
         var searchBtn=findViewById<Button>(R.id.search)
         var searchBar=findViewById<TextInputEditText>(R.id.textField)
+
+        val db = Firebase.firestore
 
         searchBtn.setOnClickListener(
             View.OnClickListener {
@@ -23,6 +28,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun search(productName: String){
+        var grid=suggestedProductsGrid()
 
+
+    }
+
+    fun suggestedProductsGrid(): GridLayout{
+        return findViewById(R.id.suggestedProducts)
     }
 }
