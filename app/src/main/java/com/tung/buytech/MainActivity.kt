66745d7df30488f38241e091758d982a8c4a9bcf
viettungvuong <viettungvuong@gmail.com>
2023-06-06 09:45:39 +0700

    Thêm code

--- app/src/main/java/com/tung/buytech/MainActivity.kt
+++ app/src/main/java/com/tung/buytech/MainActivity.kt
@@ -2,10 +2,26 @@ package com.tung.buytech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var searchBtn=findViewById<Button>(R.id.search)
+        var searchBar=findViewById<TextInputEditText>(R.id.textField)
+
+        searchBtn.setOnClickListener(
+            View.OnClickListener {
+
+            }
+        )
+    }
+
+    fun search(productName: String){
+
     }
 }
