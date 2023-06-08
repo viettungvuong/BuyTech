68c401e18e1ab973b474dbb3026150eee55d3c53
viettungvuong <viettungvuong@gmail.com>
2023-06-08 22:34:28 +0700

    Thêm image picker

--- app/src/main/java/com/tung/buytech/SellPage.kt
+++ app/src/main/java/com/tung/buytech/SellPage.kt
@@ -1,7 +1,60 @@
 package com.tung.buytech
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.HorizontalScrollView
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
 
 class SellPage  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sell_product);
 
+        val gallery=findViewById<LinearLayout>(R.id.images)
+
+        val addImageBtn=findViewById<Button>(R.id.addImageBtn)
+        addImageBtn.setOnClickListener(
+            View.OnClickListener {
+                addImageToLayout(gallery)
+            }
+        )
+    }
+
+    fun addImageToLayout(gallery: LinearLayout ){
+        val pickMultipleMedia =
+            registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
+                if (uris.isNotEmpty()) {
+                    Log.d("PhotoPicker", "Number of items selected: ${uris.size}")
+                    for (uri in uris){
+                        var newImg=ImageView(this)
+                        Glide.with(this)
+                            .load(uri)
+                            .into(newImg)
+                        val params = LinearLayout.LayoutParams(
+                            128,
+                            128 //set width và height
+                        )
+                        params.setMargins(10,10,10,10);
+                        newImg.setLayoutParams(params)
+                        gallery.addView(newImg) //thêm ảnh vào phần image view
+                    }
+                } else {
+                    Log.d("PhotoPicker", "No media selected")
+                }
+            }
+        //cho chọn tối đa 5 items
+        //define pickMultipleMedia rồi tiến hành launch
+
+        pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
+    }
 }
