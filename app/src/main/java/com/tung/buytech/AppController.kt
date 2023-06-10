418d756963a869c4ae677d19aa387aec7c075d91
viettungvuong <viettungvuong@gmail.com>
2023-06-10 14:01:58 +0700

    Create AppController.kt

---
+++ app/src/main/java/com/tung/buytech/AppController.kt
@@ -0,0 +1,12 @@
+package com.tung.buytech
+
+class AppController {
+    public class Proudct(name: String, price: Int, imageFile: String ){
+        public var name: String = name
+        public var price: Int = price
+        public var iamgeFile: String = imageFile
+        //khúc này là constructor của class
+        
+    }
+    public val List<>
+}
