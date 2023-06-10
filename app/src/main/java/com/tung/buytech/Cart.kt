d091fddcddaf9f11f4fd8b5061542b57a5270dbc
viettungvuong <viettungvuong@gmail.com>
2023-06-10 13:50:41 +0700

    Implement recycler view

--- app/src/main/java/com/tung/buytech/Cart.kt
+++ app/src/main/java/com/tung/buytech/Cart.kt
@@ -1,6 +1,20 @@
 package com.tung.buytech
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Cart: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cart)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.cartView)
+
+        val layoutManager = LinearLayoutManager(this) // or GridLayoutManager, StaggeredGridLayoutManager, etc.
+        recyclerView.layoutManager = layoutManager
+        val adapter = CartRecyclerAdapter(dataList)
+        recyclerView.adapter = adapter
+    }
 }
