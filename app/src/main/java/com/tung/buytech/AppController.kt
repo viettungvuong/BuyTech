449a2de87d3db6e12bdb9c7df41ede619c1d471e
viettungvuong <viettungvuong@gmail.com>
2023-06-10 14:12:05 +0700

    Update AppController.kt

--- app/src/main/java/com/tung/buytech/AppController.kt
+++ app/src/main/java/com/tung/buytech/AppController.kt
@@ -1,12 +1,28 @@
 package com.tung.buytech
 
+import java.util.LinkedList
+
 class AppController {
-    public class Proudct(name: String, price: Int, imageFile: String ){
+    class Product(name: String, price: Int, imageFile: String ){
         public var name: String = name
         public var price: Int = price
         public var iamgeFile: String = imageFile
         //khúc này là constructor của class
-        
+
+        fun getName(): String{
+            return this.name
+        }
+
+        fun getPrice(): Int{
+            return this.price;
+        }
+
+        fun getImage(): String{
+            return this.iamgeFile
+        }
     }
-    public val List<>
+
+
+    var personList: LinkedList<Product> = LinkedList()
+
 }
