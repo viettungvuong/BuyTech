e8663ca38eef1bd2131b7c53a703612e069dbcbc
viettungvuong <viettungvuong@gmail.com>
2023-06-10 11:07:56 +0700

    Create ItemInCart.kt

---
+++ app/src/main/java/com/tung/buytech/ItemInCart.kt
@@ -0,0 +1,27 @@
+package com.tung.buytech
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class ItemInCart @JvmOverloads constructor(
+    context: Context,
+) : LinearLayout(context) {
+
+    private val imageView: ImageView
+    private val labelTextView: TextView
+    private val priceTextView: TextView
+
+    init {
+        // Inflate the layout for the custom view
+        LayoutInflater.from(context).inflate(R.layout.view_product, this, true)
+
+        // Get references to the child views
+        imageView = findViewById(R.id.product_image)
+        labelTextView = findViewById(R.id.product_label)
+        priceTextView = findViewById(R.id.product_price)
+    }
+
+}
