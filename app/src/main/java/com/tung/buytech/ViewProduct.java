bc403c8f05f209eaa3be95d376af0bd758750176
viettungvuong <viettungvuong@gmail.com>
2023-06-14 15:14:02 +0700

    Thêm activity

---
+++ app/src/main/java/com/tung/buytech/ViewProduct.java
@@ -0,0 +1,14 @@
+package com.tung.buytech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_product);
+    }
+}
