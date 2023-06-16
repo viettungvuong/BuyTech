4f1d1036d9efbebc0aee344ecdab085f36586b00
viettungvuong <viettungvuong@gmail.com>
2023-06-16 22:26:44 +0700

    Intent get extra

--- app/src/main/java/com/tung/buytech/ViewProduct.java
+++ app/src/main/java/com/tung/buytech/ViewProduct.java
@@ -2,6 +2,7 @@ package com.tung.buytech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ViewProduct extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class ViewProduct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_product);
+
+        Intent intent=getIntent();
+        String s=intent.getStringExtra("ProductName");
+        Long price=intent.getLongExtra("ProductPrice",0);
     }
 }
