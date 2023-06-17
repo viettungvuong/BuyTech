d25808f6908184bd92ddf31e671256045338fe6c
viettungvuong <viettungvuong@gmail.com>
2023-06-17 14:51:48 +0700

    Update ProductView.kt

--- app/src/main/java/com/tung/buytech/ProductView.kt
+++ app/src/main/java/com/tung/buytech/ProductView.kt
@@ -2,15 +2,16 @@ package com.tung.buytech
 
 import android.content.Context
 import android.content.Intent
-import android.util.AttributeSet
+import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
-import com.google.api.Distribution.BucketOptions.Linear
+
 
 class ProductView @JvmOverloads constructor(
     context: Context,
@@ -20,36 +21,22 @@ class ProductView @JvmOverloads constructor(
     private val labelTextView: TextView
     private val priceTextView: TextView
 
-    private var button: Button=Button(context)
-
     private var productName: String=""
     private var productPrice: String=""
 
     private val productId: String=""
-
+    private var listener: OnClickListener? = null
     init {
 
         // Inflate the layout for the custom view
         LayoutInflater.from(context).inflate(R.layout.view_product, this, true)
-
-        var layoutParams: LinearLayout.LayoutParams=LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT)
-        button.setBackgroundColor(0) //biến button trong suốt
-        button.setOnClickListener(
-            View.OnClickListener {
-                val intent: Intent= Intent(context,ViewProduct::class.java)
-                intent.putExtra("ProductName",productName)
-                intent.putExtra("ProductPrice",productPrice)
-                intent.putExtra("ProductId",productId)
-                context.startActivity(intent) //mở intent
-            }
-        )
 
         // Get references to the child views
         imageView = findViewById(R.id.product_image)
         labelTextView = findViewById(R.id.product_label)
         priceTextView = findViewById(R.id.product_price)
 
-        addView(button) //thêm button
+        listener=clickProduct()
     }
 
     fun setProductImage(imageUrl: String) {
@@ -66,5 +53,36 @@ class ProductView @JvmOverloads constructor(
     fun setPrice(price: String) {
         productPrice=price
         priceTextView.text = price
+    }
+
+
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_UP) {
+            if (listener != null) listener!!.onClick(this)
+        }
+        return super.dispatchTouchEvent(event)
+    }
+
+
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        if (event.getAction() === KeyEvent.ACTION_UP && (event.getKeyCode() === KeyEvent.KEYCODE_DPAD_CENTER || event.getKeyCode() === KeyEvent.KEYCODE_ENTER)) {
+            if (listener != null) listener!!.onClick(this)
+        }
+        return super.dispatchKeyEvent(event)
+    }
+
+    override fun setOnClickListener(listener: OnClickListener?) {
+        this.listener = listener
+    }
+
+    fun clickProduct(): OnClickListener{
+        return View.OnClickListener {
+            val intent: Intent= Intent(context,ViewProduct::class.java)
+            intent.putExtra("ProductName",productName)
+            intent.putExtra("ProductPrice",productPrice)
+            intent.putExtra("ProductId",productId)
+            context.startActivity(intent) //mở intent
+        }
     }
 }
