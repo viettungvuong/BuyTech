c0b3b85db16450cc80f8a6959675d2a384905d4c
viettungvuong <viettungvuong@gmail.com>
2023-06-20 08:35:48 +0700

    Update AppController.kt

--- app/src/main/java/com/tung/buytech/AppController.kt
+++ app/src/main/java/com/tung/buytech/AppController.kt
@@ -19,19 +19,20 @@ class AppController {
             return this.db
         }
     }
-    open class Product(name: String, price: Int, imageFile: String ){
+    open class Product(name: String, price: Int, imageFile: String, productId: String ){
         public var name: String = name
         public var price: Int = price
         public var imageFile: String = imageFile
+        public var productId: String= productId
         //khúc này là constructor của class
 
     }
 
     //inheritance
-    class Favorite(name: String, price: Int, imageFile: String): Product(name,price,imageFile){
+    class Favorite(name: String, price: Int, imageFile: String, productId: String): Product(name,price,imageFile,productId){
         //ktra thong tin mat hang
         fun updateStatus(){
-            db.collection(collectionProducts)
+            db.collection(collectionProducts).document(productId)
         }
     }
 
