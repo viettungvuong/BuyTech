555e60018a44c997203f85ff8140ad822136eafb
viettungvuong <viettungvuong@gmail.com>
2023-06-26 21:35:27 +0700

    Update UserPage.kt

--- app/src/main/java/com/tung/buytech/UserPage.kt
+++ app/src/main/java/com/tung/buytech/UserPage.kt
@@ -85,6 +85,7 @@ class UserPage : AppCompatActivity() {
                     if (result.resultCode == Activity.RESULT_OK) {
                         val data: Intent? = result.data
                         //nếu kết quả trả về là oke
+                        
                         //ta tiến hành đổi password
                         AccountFunctions.changePassword(this, newPasswordEditText.text.toString())
                     }
