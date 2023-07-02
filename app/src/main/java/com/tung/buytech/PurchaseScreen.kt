f91760e0d3ead1a7eb4b69c70c64d85481b0cc6e
viettungvuong <viettungvuong@gmail.com>
2023-07-02 09:06:45 +0700

    Thêm cho PurchaseScreen.kt

--- app/src/main/java/com/tung/buytech/PurchaseScreen.kt
+++ app/src/main/java/com/tung/buytech/PurchaseScreen.kt
@@ -1,15 +1,53 @@
 package com.tung.buytech
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.tung.buytech.AppController.Companion.getDatabaseInstance
 
-class PurchaseScreen : AppCompatActivity() {
+class PurchaseScreen
+@JvmOverloads constructor(
+    context: Context, product: AppController.Product
+) :AppCompatActivity() {
+    companion object
+    {
+        @JvmStatic
+        lateinit var location: String
+        lateinit var seller: String
+        lateinit var phoneNumber: String
+    }
+    lateinit var currentProduct: AppController.Product
+    init { //những cái trong constructor dùng được từ init
+        val currentProduct = product
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_purchase_screen)
 
+        loadInformation(currentProduct)
+    }
+
+    fun loadInformation(product: AppController.Product){
+        val docRef = getDatabaseInstance().collection("Products").document(product.productId).get()
+
+        docRef.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document.exists()) {
+                    seller = document["seller"].toString() //set text
+                    phoneNumber = document["phoneNumber"].toString() //set text
+                    location = document["location"].toString() //set text
+                } else {
+                    Log.d("Docnodoc", "No such document")
+                }
+            } else {
+                Log.d("Docfailed", "get failed with ", task.exception)
+            }
+        }
     }
 
     //gọi cho người bán
@@ -46,3 +84,4 @@ class PurchaseScreen : AppCompatActivity() {
         return res
     }
 }
+
