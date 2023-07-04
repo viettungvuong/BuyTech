0bde6af93d707434a6d0a9d7065327679e359fbf
viettungvuong <viettungvuong@gmail.com>
2023-07-04 19:30:01 +0700

    MessageAdapter và PeopleAdapter

--- app/src/main/java/com/tung/buytech/MessageAdapter.kt
+++ app/src/main/java/com/tung/buytech/MessageAdapter.kt
@@ -1,7 +1,25 @@
 package com.tung.buytech
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import java.util.LinkedList
 
-class MessageAdapter(private val list: LinkedList<AppController.Message>): RecyclerView.Adapter<MessageHolder> {
+class MessageAdapter(private val list: LinkedList<AppController.Message>): RecyclerView.Adapter<MessageAdapter.MessageHolder>() {
+    inner class MessageHolder(val messageView: View): RecyclerView.ViewHolder(messageView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MessageHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/tung/buytech/PeopleAdapter.kt
+++ app/src/main/java/com/tung/buytech/PeopleAdapter.kt
@@ -1,8 +1,24 @@
 package com.tung.buytech
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
 
-class PeopleAdapter(private val list: LinkedList<AppController.People>): RecyclerView.Adapter<PeopleHolder>  {
+class PeopleAdapter(private val list: LinkedList<AppController.People>): RecyclerView.Adapter<PeopleAdapter.PeopleHolder>()  {
+    inner class PeopleHolder(val peopleView: View): RecyclerView.ViewHolder(peopleView){
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PeopleHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PeopleHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
