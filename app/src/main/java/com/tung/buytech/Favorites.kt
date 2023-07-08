b20790d88febcca576f2ab4cccc6476152714353
viettungvuong <viettungvuong@gmail.com>
2023-07-08 21:39:21 +0700

    Set content view FAVORITE PAGE

---
+++ app/src/main/java/com/tung/buytech/Favorites.kt
@@ -0,0 +1,12 @@
+package com.tung.buytech
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Favorites: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.favorite_page)
+    }
+}
