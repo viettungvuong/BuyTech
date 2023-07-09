03ea5a8e953f72a5a7b1b62afabf139581addf60
viettungvuong <viettungvuong@gmail.com>
2023-07-09 09:31:19 +0700

    Thêm đơn vị tiền

--- app/src/main/java/com/tung/buytech/ViewProductMain.kt
+++ app/src/main/java/com/tung/buytech/ViewProductMain.kt
@@ -43,7 +43,7 @@ class ViewProductMain : AppCompatActivity() {
         productPrice = findViewById(R.id.priceLabel)
         productDescription = findViewById(R.id.productDescription)
         productName.text = name
-        productPrice.text = AppController.reformatNumber(price)
+        productPrice.text = AppController.reformatNumber(price)+" VNĐ"
 
         //đặt hình ảnh sản phẩm
         val imgView = findViewById<ImageView>(R.id.imageView)
