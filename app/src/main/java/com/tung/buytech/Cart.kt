66116edd703691aad98a0fe2e6ea2cf5cb21954e
viettungvuong <viettungvuong@gmail.com>
2023-07-09 12:03:37 +0700

    Activity

--- app/src/main/java/com/tung/buytech/Cart.kt
+++ app/src/main/java/com/tung/buytech/Cart.kt
@@ -1,5 +1,6 @@
 package com.tung.buytech
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Cart: AppCompatActivity() {
+
     lateinit var bottomNavigationHandler: BottomNavigationHandler
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
