26c2aeff69f962876f1229bb826ca288d2dbb72f
viettungvuong <viettungvuong@gmail.com>
2023-07-16 14:10:14 +0700

    Update SwipeRecyclerHelper.kt

--- app/src/main/java/com/tung/buytech/SwipeRecyclerHelper.kt
+++ app/src/main/java/com/tung/buytech/SwipeRecyclerHelper.kt
@@ -3,16 +3,24 @@ package com.tung.buytech
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
-class SwipeRecyclerHelper: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+class SwipeRecyclerHelper(adapter: CartRecyclerAdapter): ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+    lateinit var adapter: CartRecyclerAdapter
+    init {
+        this.adapter=adapter
+    }
     override fun onMove(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder,
         target: RecyclerView.ViewHolder
     ): Boolean {
-        TODO("Not yet implemented")
+        return false //ta kh kich hoat tinh nang keo tha
     }
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        TODO("Not yet implemented")
+        val position = viewHolder.adapterPosition
+        //vi tri adapter position cua mot item trong recycler view
+
+        adapter.notifyItemChanged(position)
+        //thong bao co thay doi
     }
 }
