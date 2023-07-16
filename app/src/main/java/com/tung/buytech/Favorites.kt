8e102609951665fccbfba0a08d93fe2786d61677
viettungvuong <viettungvuong@gmail.com>
2023-07-16 14:37:12 +0700

    Update Favorites.kt

--- app/src/main/java/com/tung/buytech/Favorites.kt
+++ app/src/main/java/com/tung/buytech/Favorites.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -18,9 +19,11 @@ class Favorites: AppCompatActivity() {
         val recyclerView=findViewById<RecyclerView>(R.id.favRecyclerView)
         recyclerView.layoutManager = layoutManager
         val adapter=CartRecyclerAdapter(this,AppController.favorites)
-        val swipeHelper = SwipeRecyclerHelper(adapter,this)
-        swipeHelper.
         recyclerView.adapter=adapter
+
+        val swipeHelperCallback = SwipeRecyclerHelper(adapter,this) //cái này là callback của itemTouchHelper
+        val itemTouchHelper=ItemTouchHelper(swipeHelperCallback)
+        itemTouchHelper.attachToRecyclerView((recyclerView))
 
         var navBar=findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigationHandler=BottomNavigationHandler(this,navBar)
