5e7fbcba18f2a064fb1af116f099c92cb6e1f6a8
viettungvuong <viettungvuong@gmail.com>
2023-07-18 19:04:49 +0700

    Chat main

--- app/src/main/java/com/tung/buytech/ChatMain.kt
+++ app/src/main/java/com/tung/buytech/ChatMain.kt
@@ -2,10 +2,44 @@ package com.tung.buytech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.tung.buytech.AppController.Companion.db
 
 class ChatMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_main)
+
+        val collectionReference = db.collection("message"+Firebase.auth.currentUser!!.uid)
+
+        collectionReference.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val querySnapshot = task.result
+                if (querySnapshot != null && !querySnapshot.isEmpty) {
+                    //nếu có collection
+                    getAllPeopleMessaged(collectionReference)
+                } else {
+                    //nếu không có collection
+
+                }
+            } else {
+                Log.d("Lỗi","Lỗi")
+            }
+        }
+
+    }
+
+    //lấy danh sách tất cả người đã nhắn
+    fun getAllPeopleMessaged(collectionReference: CollectionReference){
+        collectionReference.get().addOnSuccessListener {
+            people ->
+            for (person in people){
+                //lấy từng id ra
+            }
+        }
     }
 }
