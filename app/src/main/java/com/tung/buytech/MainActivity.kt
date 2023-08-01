6e4995e75d954cfc83ba0430672b71607ffac8c4
viettungvuong <viettungvuong@gmail.com>
2023-08-01 09:13:57 +0700

    Update MainActivity.kt

--- app/src/main/java/com/tung/buytech/MainActivity.kt
+++ app/src/main/java/com/tung/buytech/MainActivity.kt
@@ -43,6 +43,12 @@ import kotlin.collections.ArrayList
 import kotlin.coroutines.suspendCoroutine
 import kotlin.math.min
 
+const val homeFragmentTag="Home"
+const val cartFragmentTag="Cart"
+const val favoriteFragmentTag="Favorite"
+const val sellFragmentTag="Sell"
+const val accountFragmentTag="Account"
+
 class MainActivity : AppCompatActivity() {
     companion object {
         @JvmField
@@ -54,9 +60,36 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var bottomNavigationHandler: BottomNavigationHandler
 
+    private fun setBottomNavChecked(){
+        //xử lý trên bottom navigation view hiển thị là đang chọn cái gì
+        if (supportFragmentManager.backStackEntryCount > 0) {
+
+            val fragment =
+                supportFragmentManager.getBackStackEntryAt(supportFragmentManager.backStackEntryCount - 1).name
+            when (fragment) {
+                homeFragmentTag -> findViewById<BottomNavigationView>(R.id.bottom_navigation).menu.findItem(
+                    R.id.home
+                ).setChecked(true)
+                cartFragmentTag -> findViewById<BottomNavigationView>(R.id.bottom_navigation).menu.findItem(
+                    R.id.cart
+                ).setChecked(true)
+                favoriteFragmentTag -> findViewById<BottomNavigationView>(R.id.bottom_navigation).menu.findItem(
+                    R.id.favorite
+                ).setChecked(true)
+                sellFragmentTag -> findViewById<BottomNavigationView>(R.id.bottom_navigation).menu.findItem(
+                    R.id.sell
+                ).setChecked(true)
+                accountFragmentTag -> findViewById<BottomNavigationView>(R.id.bottom_navigation).menu.findItem(
+                    R.id.account
+                ).setChecked(true)
+            }
+        }
+    }
+
     override fun onResume() {
         super.onResume()
 
+        setBottomNavChecked()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,15 +98,27 @@ class MainActivity : AppCompatActivity() {
 
         FirebaseApp.initializeApp(this)
         updateFavorite() //cập nhật danh sách favorite
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationHandler =
+            BottomNavigationHandler(this, bottomNavigationView) //handler bottom navigation view
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 1) {
+            supportFragmentManager.popBackStackImmediate() //quay về fragment trước trong main activity
+            //thêm chữ immediate là để cập nhật luôn
+            setBottomNavChecked()
+        } else {
+            finishAffinity()
+            System.exit(0) //thoát khỏi app luôn
+        }
+
     }
 
 }
 
-const val homeFragmentTag="Home"
-const val cartFragmentTag="Cart"
-const val favoriteFragmentTag="Favorite"
-const val sellFragmentTag="Sell"
-const val accountFragmentTag="Account"
+
 
 class BottomNavigationHandler(activity: Activity, navBar: BottomNavigationView) {
     //dùng class này để quản lý bottom nav bar gọn hơn
