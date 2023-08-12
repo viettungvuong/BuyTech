d9010155e1f21ab2a34323bf4d867775965efc36
viettungvuong <viettungvuong@gmail.com>
2023-08-12 09:32:51 +0700

    Lấy số cart

--- app/src/main/java/com/tung/buytech/control/CartController.kt
+++ app/src/main/java/com/tung/buytech/control/CartController.kt
@@ -1,10 +1,44 @@
 package com.tung.buytech.control
 
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.tung.buytech.control.AppController.Companion.cart
+import com.tung.buytech.control.AppController.Companion.db
+import com.tung.buytech.objects.Cart
+
 class CartController {
     companion object{
         @JvmField
         var numberOfCarts=0
         var numberOfOrders=0
+        var carts = ArrayList<Cart>()
+
+        @JvmStatic
+        fun retrieveAllCarts(callback: ()->Unit){
+            db.collection(collectionUsers).document(Firebase.auth.currentUser!!.uid).get().addOnSuccessListener {
+                document -> numberOfCarts=(document.getLong("number-of-carts")?:0L).toInt()
+            }
+
+            val getCart = db.collection(collectionUsers).document(Firebase.auth.currentUser!!.uid)
+                .collection("carts")
+
+            getCart.get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        if (document.id=="0"){
+                            continue //bỏ qua thằng 0
+                        }
+                        val currentCart=Cart((document.getString("content"))?:"")
+                        carts.add(currentCart!!) //thêm vào danh sách các cart
+                    }
+
+                    callback()
+                }
+        }
+
+        fun resumeCart(){
+            cart=carts[numberOfCarts]
+        }
     }
 
 }
--- app/src/main/java/com/tung/buytech/objects/Cart.kt
+++ app/src/main/java/com/tung/buytech/objects/Cart.kt
@@ -2,6 +2,7 @@ package com.tung.buytech.objects
 
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.tung.buytech.control.AppController.Companion.bindProductById
 import com.tung.buytech.control.AppController.Companion.db
 import com.tung.buytech.control.CartController
 import com.tung.buytech.control.collectionUsers
@@ -9,6 +10,18 @@ import java.util.LinkedList
 
 class Cart(): java.io.Serializable {
     var cartList = LinkedList<Product>()
+
+    constructor(str: String) : this() {
+        val splitList = str.split(',')
+
+        for (productId in splitList) {
+            bindProductById(productId) { product ->
+                if (product != null) {
+                    cartList.add(product)
+                }
+            }
+        }
+    }
 
     override fun toString(): String {
         var str = ""
@@ -22,7 +35,7 @@ class Cart(): java.io.Serializable {
         return str
     }
 
-    fun update(){
+    fun updateToFirebase(){
         val dataMap = hashMapOf(
             "content" to this.toString()
         )
