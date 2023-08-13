87fe5115d51cd54286feca12bfa7c62be3de45be
viettungvuong <viettungvuong@gmail.com>
2023-08-13 08:24:05 +0700

    Dùng flow

--- app/src/main/java/com/tung/buytech/objects/Product.kt
+++ app/src/main/java/com/tung/buytech/objects/Product.kt
@@ -1,7 +1,13 @@
 package com.tung.buytech.objects
 
 import com.tung.buytech.control.AppController
+import com.tung.buytech.control.AppController.Companion.bindProductById
+import com.tung.buytech.control.AppController.Companion.db
 import com.tung.buytech.control.collectionProducts
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 
 open class Product(var name: String, var price: Long, var imageUrl: String, var productId: String) :
     java.io.Serializable {
@@ -9,23 +15,6 @@ open class Product(var name: String, var price: Long, var imageUrl: String, var 
 
     var sold = false //đã bán hay chưa
 
-    //observer design pattern
-    fun updateSoldStatus(callback: () -> Unit) {
-        val docRef = AppController.db.collection(collectionProducts).document(productId).get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val document = task.result
-                    if (document.exists()) {
-                        val inStock = Integer.parseInt(document.getString("stock"))
-
-                        if (inStock > 0) {
-                            this.sold = true //đã bán hết sản phẩm
-                        }
-                    }
-                }
-
-            }
-    }
 
 }
 
@@ -39,16 +28,22 @@ class Favorite(name: String, price: Long, imageFile: String, productId: String) 
     ) {
         //constructor thứ hai của class
     }
+}
 
-    fun notifyMe() {
-        //thông báo khi hết hàng
-        updateSoldStatus() {
-            if (sold) {
-                var notification = "Mặt hàng đã hết :("
-                //nếu đã hết hàng thì thông báo
-            }
+class ProductRepository(private val product: Product){
+    fun updateProductSold(): Flow<Product> = flow{
+        withContext(Dispatchers.IO){
+            db.collection(collectionProducts).document(product.productId).get()
+                .addOnSuccessListener {
+                    document ->
+                    val stock = (document.getLong("stock")?:0).toInt()
+                    if (stock<=0){
+                        product.sold=true
+                    }
+                }
 
-        } //dùng hàm callback để khi nào xong rồi mới báo
+            emit(product) //phát ra product từ flow
+        }
 
     }
 }
